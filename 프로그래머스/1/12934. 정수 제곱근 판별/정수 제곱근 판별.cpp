348da08c02bc1818f3dbda047bd23ad1b6193aaf
jcmeve <jcmeve@naver.com>
2025-12-22 09:48:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -5,11 +5,8 @@ using namespace std;
 
 long long solution(long long n) {
     long long x = sqrt(n);
-    if(x*x == n){
+    if(x*x == n)
         return (x+1)*(x+1);
-    }else if((x+1)*(x+1) == n){
-        return (x+2)*(x+2);
-    }
     return -1;
 
 }
