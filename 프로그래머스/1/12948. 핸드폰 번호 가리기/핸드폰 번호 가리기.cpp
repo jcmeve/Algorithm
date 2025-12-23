3c8408fac8a2dc84f560220e5698e4fd66b1a60c
jcmeve <jcmeve@naver.com>
2025-12-23 09:29:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+
+    phone_number.replace(phone_number.begin(),phone_number.end()-4,phone_number.size()-4, '*');
+    return phone_number;
+}
