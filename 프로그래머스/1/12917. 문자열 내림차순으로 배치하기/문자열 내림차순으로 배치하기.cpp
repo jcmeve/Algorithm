880e41e50d8095ed9f078e91d2cdf55d88043495
jcmeve <jcmeve@naver.com>
2025-12-23 09:51:47 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool comp(char a, char b){return a>b;}
+string solution(string s) {
+    string answer = "";
+    sort(s.begin(), s.end(), comp);
+    return s;
+}
