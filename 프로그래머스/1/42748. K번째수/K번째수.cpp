deea6db0c76246b2b402ea3a08aee85b7f60a8ca
jcmeve <jcmeve@naver.com>
2025-12-26 09:23:16 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(int i=0;i<commands.size();i++){
+        vector<int> temp;
+        for(int j=commands[i][0]-1;j<commands[i][1];j++)
+            temp.push_back(array[j]);
+        for(int j=0;j<temp.size();j++){//sort
+            for(int k=j;k<temp.size();k++){
+                if(temp[k]<temp[j]){
+                    int T=temp[j];
+                    temp[j]=temp[k];
+                    temp[k]=T;
+                }
+            }
+        }
+        answer.push_back(temp[commands[i][2]-1]);
+    }
+    return answer;
+}
