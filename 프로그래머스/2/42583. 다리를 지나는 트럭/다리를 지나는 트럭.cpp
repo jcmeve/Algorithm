dcb1d14094e7387cfcab9d4fb0ac548fbe08bf45
jcmeve <jcmeve@naver.com>
2026-01-13 09:10:52 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.55 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,36 +1,34 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 using namespace std;
 
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 1;
+int solution(int bridge_length, int weight, vector<int> truck_weights) { 
+    int curr_weight=0;
+    int curr_time=0;
+    int answer =0 ;
+    queue<pair<int,int>> q;
+    for(int i = 0; i < truck_weights.size(); ++i){
+        curr_time++;
+        if(!q.empty() && q.front().second<=curr_time){
+            curr_weight-=q.front().first;
 
-    int curr_weights = 0;
-    queue<pair<int, int>> trucks_on_bridge;//weight,remain
-
-    for (int i = 0; i < truck_weights.size(); ++i) {
-
-
-        if (curr_weights + truck_weights[i] > weight || 
-            (!trucks_on_bridge.empty() && trucks_on_bridge.front().second == answer)) {
-            pair<int, int> t = trucks_on_bridge.front();
-            curr_weights -= t.first;
-            answer = t.second;
-            trucks_on_bridge.pop();
+            q.pop();
+        }
+        
+        
+        if(truck_weights[i]+curr_weight <=  weight){
+            q.push({truck_weights[i],curr_time+bridge_length});
+            curr_weight+=truck_weights[i];
+        }else{
             --i;
         }
-        else {
-            curr_weights += truck_weights[i];
-            trucks_on_bridge.push(pair<int, int>(truck_weights[i], answer + bridge_length));
-            cout << answer << " ~ " << answer + bridge_length << endl;
-            
-            answer++;
-        }
+
+        
     }
-
-    answer = trucks_on_bridge.back().second;
-
+    if(!q.empty()){
+        while(q.size() != 1) q.pop();
+        answer = q.front().second;
+    }
     return answer;
 }
