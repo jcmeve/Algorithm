2b9dec742a2e9a15c53da6d4ad1704418c2987a5
jcmeve <jcmeve@naver.com>
2026-01-19 09:00:45 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 83.15 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 long long solution(int w,int h) {
     long long answer = 0;
-//    f(x) = -(h/w)x + h 
-    
     for( int x = 1 ; x<w ; x++){
         int temp = (int)(-((double)h/w)*x + h) ;
         
