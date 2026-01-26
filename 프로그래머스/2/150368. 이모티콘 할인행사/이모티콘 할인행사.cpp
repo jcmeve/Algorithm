f324465546afdc06a4c4d106d808b452c5434d37
jcmeve <jcmeve@naver.com>
2026-01-26 09:29:23 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 24.95 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
+    vector<int> answer={0,0};
+    
+    //emoticons크기가 작으므로 완탐
+    int arr[]={10,20,30,40};
+    
+    for(int i = 0;i < 4<<2*(emoticons.size()-1);++i){
+        vector<int>ratio;
+        int idx = i;
+        for(int j = 0; j <emoticons.size();++j){
+            ratio.push_back(arr[idx%4]);
+            idx/=4;
+        }
+        int subCnt = 0;
+        int totalSell = 0;
+        for(auto& user : users){
+            int userSell = 0;
+            for(int j = 0;j<emoticons.size();++j){
+                if(user[0]<=ratio[j]){
+                    userSell+=emoticons[j]/100*(100-ratio[j]);
+                    if(userSell>=user[1]){
+                        subCnt++;
+                        goto SUB;
+                    }
+                }
+            }
+            totalSell+=userSell;
+            SUB:;
+        }
+        if(answer[0]<subCnt){
+            answer[0] = subCnt;
+            answer[1] = totalSell;
+        }if(answer[0] == subCnt){
+            answer[1] = answer[1]>totalSell?answer[1]:totalSell;
+        }
+        
+        
+
+    }
+    
+    
+    
+    return answer;
+}
