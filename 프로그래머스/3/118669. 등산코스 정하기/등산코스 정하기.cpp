f9fd37d6c890b6ca4a1add0ba889732d307e8069
jcmeve <jcmeve@naver.com>
2026-02-19 10:06:22 +0900

    [level 3] Title: 등산코스 정하기, Time: 5170.00 ms, Memory: 40.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+
+vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
+    vector<int> answer; 
+    vector<vector<pair<int,int>>> g(n+1);
+    vector<int> min_intensity(n+1,987654321);
+    for(auto& path : paths){
+        g[path[0]].push_back({path[1],path[2]});
+        g[path[1]].push_back({path[0],path[2]});
+    }
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+    for(int gate : gates){
+        q.push({0,gate});
+    }
+    
+    while(!q.empty()){
+        int curr_intensity = q.top().first;
+        int curr_v = q.top().second;
+        q.pop();
+        if(find(summits.begin(),summits.end(),curr_v) != summits.end()) continue;
+        
+        for(auto p : g[curr_v]){
+            if(find(gates.begin(),gates.end(),p.first) != gates.end()) continue;
+            
+            int temp_intensity = max(curr_intensity,p.second);
+            if(min_intensity[p.first] > temp_intensity){
+                min_intensity[p.first] = temp_intensity;
+                q.push({temp_intensity,p.first});
+            }
+        }
+    }
+    sort(summits.begin(), summits.end());
+    for (int summit : summits) {
+        if (answer.empty() || min_intensity[summit] < answer[1]) {
+            answer = {summit,min_intensity[summit]};
+        }
+    }
+    
+    return answer;
+}
