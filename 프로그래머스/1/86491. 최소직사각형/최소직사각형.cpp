d31e878f52641dfed789300ebc2f8fc54ecb49dd
jcmeve <jcmeve@naver.com>
2026-04-29 10:47:00 +0900

    [level 1] Title: 최소직사각형, Time: 1.49 ms, Memory: 5.02 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-    int m1=0,m2=0;
-    for(int i = 0; i < sizes.size(); ++i){
-        int a = sizes[i][0]>sizes[i][1]?sizes[i][0]:sizes[i][1];
-        int b = sizes[i][0]+sizes[i][1]-a;
-        if(m1 < a){
-            m1 = a;
+    int answer = 0;
+    int MM=-1;
+    int Mm=-1;
+    for(auto size : sizes){
+        int M = max(size[0],size[1]);
+        int m = min(size[0],size[1]);
+        if(MM<M){
+            MM=M;
         }
-        if(m2<b){
-            m2 = b;
+        if(Mm<m){
+            Mm=m;
         }
-        
-        
     }
-    return m1*m2;
+    
+    
+    return MM*Mm;
 }
