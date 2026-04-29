1b8d7cda580f076bf9a09f67d1428a0736c7726a
jcmeve <jcmeve@naver.com>
2026-04-29 11:15:32 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,46 +1,30 @@
 #include <string>
 #include <vector>
-
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
 using namespace std;
+
 
 int solution(string word) {
     int answer = 0;
+    unordered_map<char, int> m;
+    m['A']=0;
+    m['E']=1;
+    m['I']=2;
+    m['O']=3;
+    m['U']=4;
     
-    for(char& c : word){
-        switch(c){
-            case  'A':
-                c = 0;
-                break;
-            case  'E':
-                c = 1;
-                break;
-            case  'I':
-                c = 2;
-                break;
-            case  'O':
-                c = 3;
-                break;
-            case  'U':
-                c = 4;
-                break;
-            default:
-                break;
-        }
-    }
-    
-    //EIO
-    //A~AUUUU 781 -> 625 + 125 + 25 + 5 + 1
-    //E 782
-    //EIO
-    //{(E-A)* sigma(0~5-i)781 + E(1)} +  {(I-A) *  EA~EAUUU(125+25+5+1)  + EI(1) }+ {(O-A) * EIA~EIAUU(25+5+1) +1}
-    //781 + 1 + 312 + 1 + 93 + 1
-    int pows[5] = {1,5,25,125,625};
-    for(int i= 0; i < word.size(); ++i){
-        int sigma = 0;
+    for(int i =0; i < word.size(); ++i){
+        int acc = 0;
         for(int j = 0; j < 5-i; ++j){
-            sigma += pows[j];
+            acc += pow(5,j);
         }
-        answer += word[i] * sigma+1;
+        answer+=acc*m[word[i]]+1;
+        
     }
+    
+    
+    
     return answer;
 }
